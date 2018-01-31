e8adde05b0342c6183a4b208a4877bf9f2a86514
Piotr Szymaszek <piotr.szymaszek@fsi.pl>
2018-01-31 14:15:29 +0100

    Dropped support for HHVM

--- travis/install.sh
+++ travis/install.sh
@@ -4,18 +4,6 @@ set -e
 
 SYMFONY_VERSION=${SYMFONY_VERSION-2.7.*}
 COMPOSER_PREFER_LOWEST=${COMPOSER_PREFER_LOWEST-false}
-DOCKER_BUILD=${DOCKER_BUILD-false}
-
-if [ "$DOCKER_BUILD" = true ]; then
-    cp .env.dist .env
-
-    docker-compose build
-    docker-compose run --rm php composer update --prefer-source
-
-    exit
-fi
-
-composer self-update
 
 composer require --no-update --dev symfony/asset:${SYMFONY_VERSION}
 composer require --no-update symfony/framework-bundle:${SYMFONY_VERSION}
